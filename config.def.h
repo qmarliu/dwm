0ebae232f2df04fd0f078db9b93565e5e44ed708
liulei <liu6uu@163.com>
2022-03-03 10:49:23 +0800

    XK_q shortcut

--- config.def.h
+++ config.def.h
@@ -192,7 +192,7 @@ static Key keys[] = {
 	TAGKEYS(                        XK_7,                      6)
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
-	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
+	{ MODKEY|Mod1Mask|ShiftMask,    XK_q,      quit,           {0} },
 };
 
 /* button definitions */
