f7c304f3e831a7650fe8e218d646d1cf6e77ae45
liulei <liu6uu@163.com>
2022-06-23 12:42:50 +0800

    调整参数

--- config.def.h
+++ config.def.h
@@ -3,10 +3,10 @@
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
-static const unsigned int gappih    = 10;       /* horiz inner gap between windows */
+static const unsigned int gappih    = 5;       /* horiz inner gap between windows */
 static const unsigned int gappiv    = 8;       /* vert inner gap between windows */
-static const unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov    = 30;       /* vert outer gap between windows and screen edge */
+static const unsigned int gappoh    = 20;       /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov    = 20;       /* vert outer gap between windows and screen edge */
 static       int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
@@ -91,7 +91,7 @@ static const Layout layouts[] = {
 
 /* key definitions */
 #define MODKEY Mod1Mask //Alt
-// #define MODKEY Mod4Mask //Windows
+#define Windows Mod4Mask //windows
 #define TAGKEYS(KEY,TAG) \
 	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
 	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
@@ -148,22 +148,22 @@ static Key keys[] = {
 	{ MODKEY,                       XK_s,      hide,           {0} },
 	{ MODKEY|ShiftMask,             XK_s,      showone,        {0} },
 	{ MODKEY|ShiftMask,             XK_Return, zoom,           {0} },
-	{ MODKEY|Mod1Mask,              XK_u,      incrgaps,       {.i = +1 } },
-	{ MODKEY|Mod1Mask|ShiftMask,    XK_u,      incrgaps,       {.i = -1 } },
-	{ MODKEY|Mod1Mask,              XK_i,      incrigaps,      {.i = +1 } },
-	{ MODKEY|Mod1Mask|ShiftMask,    XK_i,      incrigaps,      {.i = -1 } },
-	{ MODKEY|Mod1Mask,              XK_o,      incrogaps,      {.i = +1 } },
-	{ MODKEY|Mod1Mask|ShiftMask,    XK_o,      incrogaps,      {.i = -1 } },
-	{ MODKEY|Mod1Mask,              XK_6,      incrihgaps,     {.i = +1 } },
-	{ MODKEY|Mod1Mask|ShiftMask,    XK_6,      incrihgaps,     {.i = -1 } },
-	{ MODKEY|Mod1Mask,              XK_7,      incrivgaps,     {.i = +1 } },
-	{ MODKEY|Mod1Mask|ShiftMask,    XK_7,      incrivgaps,     {.i = -1 } },
-	{ MODKEY|Mod1Mask,              XK_8,      incrohgaps,     {.i = +1 } },
-	{ MODKEY|Mod1Mask|ShiftMask,    XK_8,      incrohgaps,     {.i = -1 } },
-	{ MODKEY|Mod1Mask,              XK_9,      incrovgaps,     {.i = +1 } },
-	{ MODKEY|Mod1Mask|ShiftMask,    XK_9,      incrovgaps,     {.i = -1 } },
-	{ MODKEY|Mod1Mask,              XK_0,      togglegaps,     {0} },
-	{ MODKEY|Mod1Mask|ShiftMask,    XK_0,      defaultgaps,    {0} },
+	{ MODKEY|Windows,              XK_u,      incrgaps,       {.i = +1 } },
+	{ MODKEY|Windows|ShiftMask,    XK_u,      incrgaps,       {.i = -1 } },
+	{ MODKEY|Windows,              XK_i,      incrigaps,      {.i = +1 } },
+	{ MODKEY|Windows|ShiftMask,    XK_i,      incrigaps,      {.i = -1 } },
+	{ MODKEY|Windows,              XK_o,      incrogaps,      {.i = +1 } },
+	{ MODKEY|Windows|ShiftMask,    XK_o,      incrogaps,      {.i = -1 } },
+	{ MODKEY|Windows,              XK_6,      incrihgaps,     {.i = +1 } },
+	{ MODKEY|Windows|ShiftMask,    XK_6,      incrihgaps,     {.i = -1 } },
+	{ MODKEY|Windows,              XK_7,      incrivgaps,     {.i = +1 } },
+	{ MODKEY|Windows|ShiftMask,    XK_7,      incrivgaps,     {.i = -1 } },
+	{ MODKEY|Windows,              XK_8,      incrohgaps,     {.i = +1 } },
+	{ MODKEY|Windows|ShiftMask,    XK_8,      incrohgaps,     {.i = -1 } },
+	{ MODKEY|Windows,              XK_9,      incrovgaps,     {.i = +1 } },
+	{ MODKEY|Windows|ShiftMask,    XK_9,      incrovgaps,     {.i = -1 } },
+	{ MODKEY|Windows,              XK_0,      togglegaps,     {0} },
+	{ MODKEY|Windows|ShiftMask,    XK_0,      defaultgaps,    {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
 	{ MODKEY,                       XK_w,      killclient,     {0} },
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} }, //tile
@@ -192,7 +192,7 @@ static Key keys[] = {
 	TAGKEYS(                        XK_7,                      6)
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
-	{ MODKEY|Mod1Mask|ShiftMask,    XK_q,      quit,           {0} },
+	{ MODKEY|Windows|ShiftMask,    XK_q,      quit,           {0} },
 };
 
 /* button definitions */
